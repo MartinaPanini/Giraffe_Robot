721382f4b1f05f278b7a7ef1a8dc83a987b25e08
MartinaPanini <martipnn@gmail.com>
2025-07-09 08:41:17 -0700

    task space works (maybe)

--- catkin_ws/src/giraffe_description/src/task_conf.py
+++ catkin_ws/src/giraffe_description/src/task_conf.py
@@ -13,12 +13,13 @@ frame_name = 'ee_link'         # controlled frame
 
 # Task-space PD gains (Requirement 6)
 omega_n = 4/7                  # Natural frequency for 7s settling time (critically damped)
-Kp_task = np.eye(3) * omega_n**2  # P gain = omega_n^2
-Kd_task = np.eye(3) * 2 * omega_n  # D gain = 2*omega_n
+# Example good starting values
+Kp_task = np.diag([400, 400, 400])  # Position gain
+Kd_task = np.diag([40, 40, 40])     # Velocity gain
 
 # Postural task gains (Requirement 7)
-Kp_postural = 50.0             # Proportional gain for posture
-Kd_postural = 2 * np.sqrt(Kp_postural)  # Critically damped
+Kp_postural = 10.0                  # Postural position gain
+Kd_postural = 2.0                   # Postural velocity gain
 
 # Initial and desired configurations (Requirement 8)
 qhome = np.array([0.0, 0.0, 0.0, 0.0, 0.0])  # Homing configuration
@@ -26,4 +27,4 @@ q0 = np.array([0.0, -np.pi/2, 0.0, 0.0, -np.pi/2])  # Reference posture
 pdes = np.array([1.0, 2.0, 1.0])  # Desired position [x,y,z]
 
 # Trajectory parameters
-traj_duration = 3.0            # Time to reach target
+traj_duration = 5.0            # Time to reach target
